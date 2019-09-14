05c5ad557728e13b9469dab57d038a350de6c3e3
Brent Miller <brentwmiller93@gmail.com>
2019-09-14 01:36:28 -0400

    implements movement via 'wasd'

--- src/hero/hero.js
+++ src/hero/hero.js
@@ -1,18 +1,66 @@
-import { Bodies } from 'matter-js';
+import { Events } from 'matter-js';
+import { PersonEntity } from '../person-entity/person-entity';
 
-export class Hero {
-  constructor(id, position, size) {
-    this.id = id;
-    this.position = position;
-    this.size = size;
+export class Hero extends PersonEntity {
+  constructor(engine, id, position, size) {
+    super(engine, id, position, size);
 
-    this.options = {
-      id: this.id,
-      fillStyle: '#ff0000',
+    this.directionCodes = [65, 87, 68, 83];
+    this.speed = 0.01;
+
+    // Movement: W, A, S, D | x, y
+    this.direction = {
+      87: { id: 'up', x: 0, y: -1 },
+      65: { id: 'left', x: -1, y: 0 },
+      83: { id: 'down', x: 0, y: 1 },
+      68: { id: 'right', x: 1, y: 0 },
     };
+
+    this.keys = [];
   }
 
-  create() {
-    return Bodies.circle(this.position.x, this.position.y, this.size, this.options);
+  birth() {
+    this._handleMovement();
+    this.matter.mass = 1;
+    this.matter.frictionAir = 0.5;
+  }
+
+  _handleMovement() {
+    window.addEventListener('keydown', (e) => {
+      const index = this.keys.indexOf(e.keyCode);
+
+      if (index === -1) {
+        this.keys.push(e.keyCode);
+      }
+    });
+
+    window.addEventListener('keyup', (e) => {
+      const index = this.keys.indexOf(e.keyCode);
+
+      if (index > -1) {
+        this.keys.splice(index, 1);
+      }
+    });
+
+    this._move();
+  }
+
+  _move() {
+    Events.on(this.engine, 'beforeTick', () => {
+      if (this.keys.length === 0) {
+        return;
+      }
+
+      this.keys.forEach((key) => {
+        if (this.directionCodes.includes(key)) {
+          const move = this.direction[key];
+
+          const x = move.x * this.speed;
+          const y = move.y * this.speed;
+
+          this.matter.force = { x, y };
+        }
+      });
+    });
   }
 }
