1a36c6599d4f689a4960f6884d2f6a81f8903702
Brent Miller <brentwmiller93@gmail.com>
2019-10-05 12:34:56 -0400

    Reworks draw to include sprite

--- src/person-entity.ts
+++ src/person-entity.ts
@@ -1,5 +1,6 @@
-import { Bodies } from 'matter-js';
+import { Bodies, IBodyRenderOptionsSprite, IBodyRenderOptions } from 'matter-js';
 import WorldBuilder from './world-builder';
+import { ImageLoader } from './image-loader';
 
 export interface IPersonEntityPosition {
   x: number;
@@ -12,13 +13,16 @@ export class PersonEntity {
   position: IPersonEntityPosition;
   size: number;
   options: Matter.IBodyDefinition;
-  matter: Matter.Bodies;
+  matter: Matter.Body;
+  sprite: any;
+  ready: boolean = false;
 
-  constructor(label: string, position: IPersonEntityPosition, size: number) {
+  constructor(label: string, position: IPersonEntityPosition, size: number, sprite: any) {
     this.engine = WorldBuilder.getEngine();
     this.label = label;
     this.position = position;
     this.size = size;
+    this.sprite = sprite;
 
     this.options = {
       label: this.label,
@@ -28,6 +32,22 @@ export class PersonEntity {
   }
 
   draw() {
-    this.matter = Bodies.circle(this.position.x, this.position.y, this.size, this.options);
+    return new Promise(async (resolve) => {
+      const url: any = await new ImageLoader().loadImage(this.sprite);
+      console.log(url);
+
+      this.options.render = {
+        sprite: {
+          texture: url,
+          xScale: this.size,
+          yScale: this.size,
+        },
+      };
+
+      this.matter = Bodies.rectangle(this.position.x, this.position.y, this.size, this.size, this.options);
+      WorldBuilder.addToWorld(this.matter);
+
+      resolve(true);
+    });
   }
 }
