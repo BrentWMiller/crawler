9a72b64662ac6d4bd7b7157bfde06e6957eb3ecd
Brent Miller <brentwmiller93@gmail.com>
2019-10-06 13:16:48 -0400

    Moves hero init into world builder

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,4 @@
 import WorldBuilder from './world-builder';
-import { Hero } from './hero';
-import * as SpriteSheet from './assets/sprite-sheet.png';
-import { ISprite } from './animation/animation.interface';
-import { wizardAnimations } from './animation/wizard';
 
 window.addEventListener('load', (event) => {
   init();
@@ -10,18 +6,4 @@ window.addEventListener('load', (event) => {
 
 const init = async () => {
   WorldBuilder.init();
-
-  const heroPosition = {
-    x: 400,
-    y: 400,
-  };
-
-  const heroSprite: ISprite = {
-    url: SpriteSheet,
-    animations: wizardAnimations,
-  };
-
-  let hero = new Hero('player', heroPosition, 16, heroSprite);
-  await hero.draw();
-  hero.birth();
 };
